339618d678b4a66749bb3b37a8f8943288939095
oe.sonnh <sonnh.90@gmail.com>
2020-03-03 10:34:05 +0700

    Minor: change default value of debug config

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,5 +1,5 @@
 export const envConfig = {
-  debug: process.env.HUBOT_SCHEDULE_DEBUG || 1,
+  debug: process.env.HUBOT_SCHEDULE_DEBUG || 0,
   chainReceiver: process.env.HUBOT_SCHEDULE_CHAIN_RECEIVER || '1',
   ignoreExternalControl: process.env.HUBOT_SCHEDULE_IGNORE_EXTERNAL_CONTROL || 0,
   utcOffset: process.env.HUBOT_SCHEDULE_UTC_OFFSET || '+00:00',
